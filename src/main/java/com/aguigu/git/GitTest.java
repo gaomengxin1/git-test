05a5f8652ec1cab4f947da888bae3c8f4002fbb3
gaomengxin1 <2901846898@qq.com>
2022-03-11 15:32:25 +0800

    hot-fix-first-commit

--- src/main/java/com/aguigu/git/GitTest.java
+++ src/main/java/com/aguigu/git/GitTest.java
@@ -8,11 +8,11 @@ public class GitTest {
 
     public static void main(String[] args) {
 
-        System.out.println("hello git3");
-        System.out.println("hello git3");
-        System.out.println("hello git3");
-        System.out.println("hello git3");
-        System.out.println("hello git3");
-        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
+        System.out.println("hello git4");
+        System.out.println("hello git4");
+        System.out.println("hello git4");
     }
 }
