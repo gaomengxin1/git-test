a10c2f3143d335f6e3635b3197298ae2594843b3
gaomengxin1 <2901846898@qq.com>
2022-03-11 16:34:01 +0800

    push-test

--- src/main/java/com/aguigu/git/GitTest.java
+++ src/main/java/com/aguigu/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hello git4");
         System.out.println("hot-fix");
+        System.out.println("push test");
     }
 }
